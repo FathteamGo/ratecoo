309923b58ffd88bcb17ee6be72546ffc9bfe915b
Cecep Saeful Azhar Hidayat, ST <122300501+cecep-azhar@users.noreply.github.com>
2025-12-06 14:14:11 +0700

    add package

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    allowedDevOrigins: [
+      'localhost',
+      '127.0.0.1',
+      '0.0.0.0',
+      '169.254.83.107',
+    ],
+  },
 };
 
 export default nextConfig;
